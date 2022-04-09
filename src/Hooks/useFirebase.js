6d5c7cca503d5e0c883b805c3c944169af38545c
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-09 22:27:00 +0600

    declare many components

---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    useEffect( () =>{
+
+    },[]);
+
+    const signInWithGoogle = () => {
+        console.log('sign In here');
+    }
+
+    return [user, setUser];
+}
+
+export default useFirebase;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-const Login = () => {
+const Register = () => {
     return (
         <div>
-            <h3>Login here.</h3>
-            <div style={{margin: '20px'}}>
-                <button>Google Sign In</button>
-            </div>
-            
+            <h3>Please register now</h3>
             <form>
+                <input type="text" placeholder='your name' />
+                <br />
                 <input type="email" placeholder='your email' />
                 <br />
                 <input type="password" name="" id="" placeholder='password please' />
                 <br />
-                <input type="Submit" value="Login" />
+                <input type="Submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
