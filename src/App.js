ebb333103a6eabc1493e71a0bdefbe6a40849067
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-10 12:14:13 +0600

    require auth ser

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Orders from './components/Orders/Orders';
 import Products from './components/Products/Products';
 import Register from './components/Register/Register';
+import RequireAutrh from './components/RequireAuth/RequireAutrh';
 
 function App() {
   return (
@@ -15,6 +17,11 @@ function App() {
          <Route path='/login' element={<Login></Login>}></Route>
          <Route path='/register' element={<Register></Register>}></Route>
          <Route path='/products' element={<Products></Products>}></Route>
+         <Route path='/orders' element={
+              <RequireAutrh>
+                <Orders></Orders>
+              </RequireAutrh>
+         }></Route>
        </Routes>
     </div>
   );
